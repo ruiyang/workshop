bf3f14cf744326ddb1a78d079addef2ae3665c47
Rui Yang <ryang@bglcorp.com.au>
2018-05-04 14:57:23 +1000

    clone react demo

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,6 +1,6 @@
 mkdir jenkins_home
 docker run --name jenkins -d -p 8080:8080 -p 50000:50000 -v $(pwd)/jenkins_home:/var/jenkins_home jenkins/jenkins:lts
-
+git clone https://github.com/ruiyang/react_demo.git
 #cp ~/.ssh/id_rsa.pub $(pwd)/git-server/keys
 #docker build -t jenkins-bgl-alpine:1.0 .
 #mkdir -p git-server/keys && mkdir -p git-server/repos
